a44ad32275105d428609016b4b3f606e04adf494
Mikhai1tal <opp2599@naver.com>
2022-10-03 22:41:10 +0900

    twice

--- First/lib/main.dart
+++ First/lib/main.dart
@@ -10,10 +10,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: Center(
-        child: Container( width: 50, height: 50, color: Colors.blue ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('This is app')),
+        body: Container(
+            child: Text('hey')),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+          )
+        ),
       )
     );
-
   }
 }
